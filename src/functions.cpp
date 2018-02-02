aa71772f1c25d76c323e2f8ff6a5a743843ca8ea
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-02 16:36:14 +0100

    Add loop for drawing

--- src/functions.cpp
+++ src/functions.cpp
@@ -4,18 +4,10 @@ using namespace std;
 
 void draw()
 {
-	cout << "-------------------------\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "|\t\t\t|\n";
-	cout << "-------------------------\n";
+	for (int i=0; i != 13; ++i)
+	{
+		if (i % 12 == 0)
+			cout << "-------------------------\n";
+		else cout << "|\t\t\t|\n";
+	}
 }
