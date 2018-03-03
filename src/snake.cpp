5f05e014edace7a35fcfbf2c4acb9b1ea8a8a1ed
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-03 13:44:50 +0100

    Remove init() and draw() definitions

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,19 +1,1 @@
 #include "../include/snake.h"
-
-void init()
-{
-    for (auto& a : board)
-        a = 'X';
-}
-
-void draw()
-{
-    for (int i=0; i!=board.size(); ++i)
-        if (!(i%20))
-            std::cout << board[i];
-        else
-        {
-            std::cout << endl;
-            std::cout << board[i];
-        }
-}
