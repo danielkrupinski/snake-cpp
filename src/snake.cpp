967732a953bddd79079a49085311d3405798c5e1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-03 14:16:52 +0100

    Add drawing algorithm

--- src/snake.cpp
+++ src/snake.cpp
@@ -3,11 +3,15 @@
 void Snake::init()
 {
     for (int i=0; i!=920; ++i)
-        board.push_back('X');
+        if (i<40 || i>880)
+            board.push_back('-');
+        else if (!(i%39))
+            board.push_back('|');
+        else board.push_back(' ');
 }
 
 void Snake::draw()
 {
     for (auto& a : board)
-        std::cout << a << ' ';
+        std::cout << a;
 }
