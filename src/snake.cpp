adfd5520d490a5abf80c1313b560cae52065d336
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-03 14:37:36 +0100

    Fix draw() function

--- src/snake.cpp
+++ src/snake.cpp
@@ -3,9 +3,9 @@
 void Snake::init()
 {
     for (int i=0; i!=1880; ++i)
-        if (i<80)
+        if (i<80 || i>1840)
             board.push_back('-');
-        else if (i%78==0)
+        else if (i%80==0 || (i-159)%80==0)
             board.push_back('|');
         else board.push_back(' ');
 }
